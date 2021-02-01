b642783b2fb186ff82004dcb6a823824cd1e97a4
Jeremiah Adeleye <jerryb.adeleye@gmail.com>
2021-02-01 20:06:36 +0100

    Added new solutions

--- BinaryGap.java
+++ BinaryGap.java
@@ -1,15 +1,23 @@
 package javaSolutions;
 
-public class BinaryGap {
+import java.util.Scanner;
 
-    private static int solution(int N) {
+public class BinaryGap {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Number: ");
+        int number = scanner.nextInt();
+        System.out.println(getBinaryGap(number));
+    }
+    private static int getBinaryGap(int number) {
         int max_val = Integer.MIN_VALUE;
-        String binNumber = numberToBin(N);
+        String binNumber = intToBin(number);
         int value = 0;
         char[] bins = binNumber.toCharArray();
-        for (char c : bins) {
-            if (c == '0') value+=1;
-            else if (c == '1' && value > max_val){
+        for (char binChar : bins) {
+            if (binChar == '0') value+=1;
+            else if (binChar == '1' && value > max_val){
                 max_val = value;
                 value = 0;
             } else {
@@ -19,18 +27,14 @@ public class BinaryGap {
         return max_val;
     }
 
-    private static String numberToBin(int N) {
-        if (N == 0) return "O";
+    private static String intToBin(int integer) {
+        if (integer == 0) return "O";
         StringBuilder bin = new StringBuilder();
-        while (N > 0) {
-            String digit = ((N & 1) == 1) ? "1" : "0";
+        while (integer > 0) {
+            String digit = ((integer & 1) == 1) ? "1" : "0";
             bin.insert(0, digit);
-            N = Math.floorDiv(N, 2);
+            integer = Math.floorDiv(integer, 2);
         }
         return bin.toString();
-    }
-
-    public static void main(String[] args) {
-        System.out.println(solution(6291457));
     }
 }
