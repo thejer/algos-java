b7974d49b9b47ecd9af974ac848d261e5c74cbe0
thejer <jerryb.adeleye@gmail.com>
2021-09-03 11:16:30 +0100

    Solved reverse string

--- TwoSumSorted.java
+++ TwoSumSorted.java
@@ -4,20 +4,16 @@ import java.util.Arrays;
 
 public class TwoSumSorted {
     public static int[] twoSum(int[] numbers, int target) {
-        int [] result = new int[2];
         int start = 0, end = numbers.length-1;
-
         while (start < end) {
             int sum = numbers[start] + numbers[end];
             if (sum < target) start++;
             else if (sum > target) end --;
             else {
-                result[0] = start+1;
-                result[1] = end+1;
-                return result;
+                return new int[]{start+1, end+1};
             }
         }
-        return result;
+        return new int [] {0,0};
     }
 
     public static void main(String[] args) {
