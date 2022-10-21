6a95f9cd9a3f2118d1e0b5448a716afd9edda3b8
Jerry Adeleye <jerryb.adeleye@gmail.com>
2022-10-21 13:14:22 +0100

    Solved BackspaceCompare, DutchFlag, SubarrayProductLessThanK Sliding window and two pointer, TripletWithSmallerSum

---
+++ Patterns/SlidingWindow/SubarrayProductLessThanK.java
@@ -0,0 +1,37 @@
+package javaSolutions.Patterns.SlidingWindow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class SubarrayProductLessThanK {
+
+    public static List<List<Integer>> findSubarrays(int[] arr, int target) {
+        List<List<Integer>> subarrays = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < target) {
+                List<Integer> tempList = new ArrayList<>();
+                tempList.add(arr[i]);
+                subarrays.add(tempList);
+                int prod = arr[i];
+                if (i == arr.length - 1) break;
+                int j = i + 1;
+                while (prod < target && j < arr.length) {
+                    prod *= arr[j];
+                    if (prod < target) {
+                        List<Integer> sublist = new ArrayList<>(subarrays.get(subarrays.size() - 1));
+                        sublist.add(arr[j++]);
+                        subarrays.add(new ArrayList<>(sublist));
+                    }
+                }
+            }
+        }
+
+        return subarrays;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(SubarrayProductLessThanK.findSubarrays(new int[]{2, 5, 3, 10}, 30));
+        System.out.println(SubarrayProductLessThanK.findSubarrays(new int[]{8, 2, 6, 5}, 50));// [8], [8,2], [2], [2,6], [6], [6,5], [5]
+    }
+}
---
+++ Patterns/TwoPointers/SubarrayProductLessThanK.java
@@ -0,0 +1,29 @@
+package javaSolutions.Patterns.TwoPointers;
+
+import java.util.*;
+
+class SubarrayProductLessThanK {
+
+  public static List<List<Integer>> findSubarrays(int[] arr, int target) {
+    List<List<Integer>> result = new ArrayList<>();
+    double product = 1;
+    int left = 0;
+    for (int right = 0; right < arr.length; right++) {
+      product *= arr[right];
+      while (product >= target && left <= right)
+        product /= arr[left++];
+
+      List<Integer> tempList = new LinkedList<>();
+      for (int i = right; i >= left; i--) {
+        tempList.add(0, arr[i]);
+        result.add(new ArrayList<>(tempList));
+      }
+    }
+    return result;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(SubarrayProductLessThanK.findSubarrays(new int[] { 2, 5, 3, 10 }, 30));
+    System.out.println(SubarrayProductLessThanK.findSubarrays(new int[] { 8, 2, 6, 5 }, 50));
+  }
+}
